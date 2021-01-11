ab8315a00bf2960c49bbe3e3ee0f87cc9384eaab
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-01-11 18:20:15 +0900

    PUSH

---
+++ script.js
@@ -0,0 +1,18 @@
+// set Slick Slider
+$('.slider-nav').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,  
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// // Font Load
+// WebFont.load({
+//   google: {
+//     families: ['Noto Sans KR', 'sans-serif']
+//   }  
+// });  
+
+
