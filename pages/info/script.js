d70f5509b59651aeae1297eff473e67cf033404d
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-01-17 21:28:26 +0900

    2

---
+++ pages/info/script.js
@@ -0,0 +1,35 @@
+// set Slick Slider
+$('.slider-nav').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,  
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// $('.slider-nav').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 1,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: true,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: true,
+//         centerMode: true,
+//         centerPadding: '100px',
+//         slidesToShow: 3
+//       }
+//     }
+//   ]
+// });
