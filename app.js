1909690285ea417f934e9639b624d743eac185df
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-01-22 22:04:14 +0900

    fix app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+var PORT = 2393;
+
+app.use(cors());
 
 app.get('/', async(req, res, next)=>{
 	res.sendFile(__dirname + '/index.html');
@@ -9,7 +13,32 @@ app.get('/style.css', async(req, res, next)=>{
 	res.sendFile(__dirname + '/style.css');
 });
 
-var PORT = 2393;
+app.get('/script.js', async(req, res, next)=>{
+	res.sendFile(__dirname + '/script.js');
+});
+
+app.get('/src/main.png', async(req, res, next)=>{res.sendFile(__dirname + '/src/main.png');});
+app.get('/src/poster_jieun.png', async(req, res, next)=>{res.sendFile(__dirname + '/src/poster_jieun.png');});
+app.get('/src/poster_project.png', async(req, res, next)=>{res.sendFile(__dirname + '/src/poster_project.png');});
+
+
+
+
+app.get('/pages/slide/index.html', async(req, res, next)=>{
+	res.sendFile(__dirname + '/pages/slide/index.html');
+});
+
+app.get('/pages/slide/style.css', async(req, res, next)=>{
+	res.sendFile(__dirname + '/pages/slide/style.css');
+});
+
+app.get('/pages/slide/script.js', async(req, res, next)=>{
+	res.sendFile(__dirname + '/pages/slide/script.js');
+});
+
+app.get('/src/BG.jpg', async(req, res, next)=>{res.sendFile(__dirname + '/src/BG.jpg');});
+app.get('/src/paperbg.png', async(req, res, next)=>{res.sendFile(__dirname + '/src/paperbg.png');});
+
 app.listen(process.env.PORT || 2393, () => {
 	console.log(`Server Works !!! At port`+' '+PORT);
 });
