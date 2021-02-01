e586b93d7f38d56ee49bb9e0ea48926df6a90673
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-02-02 02:49:57 +0900

    add media query

--- pages/slide/asphalt/script.js
+++ pages/slide/asphalt/script.js
@@ -1,15 +1,8 @@
-var webAppend = (num) => {
-  eval('var chck = web.w'+num);
-  document.getElementsByClassName('txt__title')[num].innerHTML = chck.title;
-  document.getElementsByClassName('txt__Box')[num].innerHTML = chck.text;
-};
-
 // MENU TAB
+var menuBlank = document.getElementsByClassName('menu__blank')[0];
+var menuTab = document.getElementsByClassName('menu__tab')[0];
+var menu = document.getElementsByClassName('menu')[0];
 var menuExit = () => {
-  var menuBlank = document.getElementsByClassName('menu__blank')[0];
-  var menuTab = document.getElementsByClassName('menu__tab')[0];
-  var menu = document.getElementsByClassName('menu')[0];
-
   menuTab.setAttribute('style', 'margin-left: 100%; opacity: 0%; z-index: 0; transition: .5s;');
   menuBlank.setAttribute('style', 'right: 100%; opacity: 0%; z-index: 0; transition: .5s;');
   setTimeout(function() {
@@ -17,16 +10,19 @@ var menuExit = () => {
   }, 0300); //1000 = 1s
 }
 var menuOpen = () => {
-  var menuBlank = document.getElementsByClassName('menu__blank')[0];
-  var menuTab = document.getElementsByClassName('menu__tab')[0];
-  var menu = document.getElementsByClassName('menu')[0];
-
   menu.setAttribute('style', 'z-index: 20;');
   menuTab.setAttribute('style', 'margin-left: 30%; opacity: 100%; z-index: 20; transition: .5s;');
   menuBlank.setAttribute('style', 'right: 70%; opacity: 70%; z-index: 20; transition: .5s;');
 }
 
-window.onload = () =>{
+
+var webAppend = (num) => {
+  eval('var chck = web.w'+num);
+  document.getElementsByClassName('txt__title')[num].innerHTML = chck.title;
+  document.getElementsByClassName('txt__Box')[num].innerHTML = chck.text;
+};
+
+window.onload = () => {
   for(var i=1; i < 7; i++){webAppend(i);}
 };
 
@@ -41,3 +37,4 @@ $('.slider-nav').slick({
   autoplay: false,
   autoplaySpeed: 2000,  
 });
+
